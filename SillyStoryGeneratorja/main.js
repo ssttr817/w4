e38677a03164d6d822358ad649de311f08554d22
ssttr817 <95006296+ssttr817@users.noreply.github.com>
2021-12-09 15:35:15 +0900

    Update main.js

--- SillyStoryGeneratorja/main.js
+++ SillyStoryGeneratorja/main.js
@@ -7,9 +7,9 @@ function randomValueFromArray(array){
   return array[random];
 }
 
-const storyText = 'その日は熊谷並みに暑かった。, so :insertx: went for a walk. When they got to :inserty:, they stared in horror for a few moments, then :insertz:. Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
+const storyText = 'その日は凍るくらい寒かった。だから :insertx: はかなりの厚着をした。:inserty:で見た猫は冬毛でふわふわで、:insertz:は暖かそうで羨ましいと思った。 Bob saw the whole thing, but was not surprised — :insertx: weighs 300 pounds, and it was a hot day.';
 const insertX = ['Willy the Goblin', 'Big Daddy', 'Father Christmas'];
-const insertY = ['the soup kitchen', 'Disneyland', 'the White House'];
+const insertY = ['公園', '道端', '街中'];
 const insertZ = ['spontaneously combusted', 'melted into a puddle on the sidewalk', 'turned into a slug and crawled away'];
 
 randomize.addEventListener('click', result);
